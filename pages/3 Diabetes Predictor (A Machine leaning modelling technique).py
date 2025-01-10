c87e7d595863e6b5512a593ef416a9a633b544e2
lawrenceokolo <lawrenceobabc@gmail.com>
2025-01-09 22:55:20 -0800

    Update 3 Diabetes Predictor (A Machine leaning modelling technique).py

--- pages/3 Diabetes Predictor (A Machine leaning modelling technique).py
+++ pages/3 Diabetes Predictor (A Machine leaning modelling technique).py
@@ -2,3 +2,11 @@ import streamlit as st
 
 
 st.title("Diabetes Predictor(In Progress)")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/beautiful columbians.webp")
+
+
+with col2:
+    st.image("images/friendly humainoid csv.webp")
