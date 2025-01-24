fd485cd19655015ae5ca6d59494e94ff40fd4a8f
lawrenceokolo1 <okololawrence48@yahoo.com>
2025-01-23 23:04:37 -0800

    updated

--- pages/3 Diabetes Predictor (A Machine leaning modelling technique).py
+++ pages/3 Diabetes Predictor (A Machine leaning modelling technique).py
@@ -2,7 +2,8 @@ import streamlit as st
 import joblib
 import pandas as pd
 import numpy as np
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM\
+
 
 st.title("Diabetes Predictor(In Progress)") 
 st.write('''The Diabetes Predictor Project is a machine learning-based application designed to predict the likelihood of diabetes in patients based on key health metrics and symptoms. Using features such as glucose level, HbA1c, 
