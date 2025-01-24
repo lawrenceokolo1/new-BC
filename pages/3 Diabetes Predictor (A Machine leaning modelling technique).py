8d069f7011758d5f4a53497e9b9171916173317e
lawrenceokolo1 <okololawrence48@yahoo.com>
2025-01-24 10:31:49 -0800

    updated

--- pages/3 Diabetes Predictor (A Machine leaning modelling technique).py
+++ pages/3 Diabetes Predictor (A Machine leaning modelling technique).py
@@ -6,7 +6,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 st.title("Diabetes Predictor(In Progress, 50% ****)") 
-st.info("by lawrence okolo ")
+st.caption("by lawrence okolo ")
 
 st.write('''The Diabetes Predictor Project is a machine learning-based application designed to predict the likelihood of diabetes in patients based on key health metrics and symptoms. Using features such as glucose level, HbA1c, 
 blood pressure, insulin, cholesterol, and symptoms like polyuria (frequent urination) and polydipsia (excessive thirst), the model provides a probabilistic classification of whether a patient is diabetic or not.''')
